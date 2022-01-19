ce386fee87e61fa2ee74c1d28a0e49f8d97772a1
Felipe <santinha_sarinhax23@outlook.com>
2022-01-19 13:37:52 -0400

    primeiro hello word do curso de php udemy alterado 2

--- index.php
+++ index.php
@@ -3,8 +3,31 @@
 echo 'hello word';
 
 /*
+1- criando a estrutura do git em uma pasta
+no windows (cria uma pasta chamada .git normalmente esta oculta):
+
+git init 
+
+
+2-add  a pasta do do repostirio do github no
+ seu repositorio local no git:
+
+ git clone https://nome/repositorio
+
+ 3-add  seu servidor de origem que e o  seu repositorio local:
+
 git remote add origin https://github.com/felipeno22/php.git
- add  seu servidor de origem que e o  seu repositorio no caso
+ 
+4- ADD O ARQUIVO PARA A UMA AREA DE ESTAGIO TEMPORARIA(STAGE AREA)
+
+git add nome_arquivo_especifico
+
+obs: vc  se quiser add mais de um arquivo , seja um q  criou ,alterou ou ate quando excluir
+ git add --all 
+
+ 5- para salvar suas alterações ,exclusoes
+git commit -m 'escreva sempre aqui o q  foi feito' 
+
 */
 ?>
 
